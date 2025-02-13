44c24a525c9a3cab6ef6731aec069fd504608cbf
TryCatchB <pozdrakovzena@gmail.com>
2025-02-13 21:04:23 +0300

    Update configure.swift

--- server/Sources/configure.swift
+++ server/Sources/configure.swift
@@ -2,17 +2,25 @@ import Vapor
 import Fluent
 import FluentPostgresDriver
 
-
-public func configure(_ app: Application) throws {
-    // Настроим подключение к базе данных PostgreSQL с учетом последних версий
-    app.databases.use(.postgres(
-        configuration: SQLPostgresConfiguration(
+public func configure(_ app: Application) async throws {
+    // Настроим подключение к базе данных PostgreSQL
+    app.databases.use(
+        .postgres(
             hostname: "localhost",  // Адрес сервера PostgreSQL
-            port: 5432, // Порт
+            port: 5432,             // Порт
             username: "postgres",   // Имя пользователя базы данных
             password: "0000",       // Пароль
-            database: "postgres",    // Имя базы данных
-            tls: .prefer(try .init(configuration: .clientDefault))
-        )
-    ), as: .psql) // Подключаем как PostgreSQL
+            database: "postgres"   // Имя базы данных
+        ),
+        as: .psql
+    )
+    
+    // Регистрация миграций
+    app.migrations.add(CreateChat())
+
+    // Применение миграций
+    try await app.autoMigrate()
+    
+    // Регистрация контроллеров
+    try app.register(collection: ChatController())
 }
