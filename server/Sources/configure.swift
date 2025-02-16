c5f6f1e639fd55a0899ffc3c28536a9cb2038f26
TryCatchB <pozdrakovzena@gmail.com>
2025-02-16 18:37:14 +0300

    Update configure.swift

--- server/Sources/configure.swift
+++ server/Sources/configure.swift
@@ -11,6 +11,15 @@ public func configure(_ app: Application) throws {
         database: "edelweiss_chat"
     ), as: .psql)
 
+    // Добавляем CORS Middleware
+    let corsConfig = CORSMiddleware(configuration: .init(
+        allowedOrigin: .custom("http://localhost:3000"), // Разрешаем запросы с фронтенда
+        allowedMethods: [.GET, .POST, .PUT, .DELETE, .OPTIONS], // Разрешённые HTTP-методы
+        allowedHeaders: [.accept, .contentType, .origin, .authorization]
+    ))
+    app.middleware.use(corsConfig)
+
+    // Добавляем миграции
     app.migrations.add(CreateChat(), CreateMessage())
     try app.autoMigrate().wait()
 
