6eca46b7e9b09d5005120464b6e8b526d71f6ead
TryCatchB <pozdrakovzena@gmail.com>
2025-02-16 18:37:44 +0300

    Update MessageController.swift

--- server/Sources/Controllers/MessageController.swift
+++ server/Sources/Controllers/MessageController.swift
@@ -3,38 +3,32 @@ import Vapor
 
 struct MessageController: RouteCollection {
     func boot(routes: RoutesBuilder) throws {
-        // Группировка маршрутов для сообщений
         let messages = routes.grouped("chats", ":chatId", "messages")
-        messages.post(use: create)  // Создание сообщения
-        messages.get(use: getAllMessages) // Получение всех сообщений
+        messages.post(use: create)
+        messages.get(use: getAllMessages)
     }
 
-    // Функция для создания сообщения в чате
     func create(req: Request) async throws -> Message {
         let messageData = try req.content.decode(CreateMessageRequest.self)
-        
-        // Проверяем, существует ли чат с таким chatId
+
         guard let _ = try await Chat.find(messageData.chatId, on: req.db) else {
             throw Abort(.notFound, reason: "Chat not found")
         }
 
-        // Создаем и сохраняем новое сообщение
-        let message = Message(content: messageData.content, chatId: messageData.chatId)
+        let message = Message(content: messageData.content, createdAt: Date(), chatId: messageData.chatId)
         try await message.save(on: req.db)
 
         return message
     }
 
-    // Функция для получения всех сообщений из чата
     func getAllMessages(req: Request) async throws -> [Message] {
-        // Извлекаем chatId из параметров запроса
         guard let chatId = req.parameters.get("chatId", as: UUID.self) else {
             throw Abort(.badRequest, reason: "Invalid chat id")
         }
 
-        // Получаем все сообщения для этого чата
         return try await Message.query(on: req.db)
             .filter(\.$chatId == chatId)
+            .sort(\.$createdAt, .ascending)
             .all()
     }
 }
