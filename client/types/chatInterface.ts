0b3d628190e941688106fd327dccf43d6c068d18
TryCatchB <pozdrakovzena@gmail.com>
2025-02-17 18:43:10 +0300

    Update chatInterface.ts

--- client/types/chatInterface.ts
+++ client/types/chatInterface.ts
@@ -1,5 +1,39 @@
+import { Dispatch, SetStateAction } from "react";
+
 export interface Chat {
   id: number;
   title: string;
-  messages: { text: string; isError?: boolean }[];
+  messages: { content: string; isError?: boolean }[];
 }
+
+export interface BaseSetChats {
+  setChats: Dispatch<SetStateAction<Chat[]>>;
+}
+
+export interface BaseChatProps extends BaseSetChats {
+  chats: Chat[];
+}
+
+export interface WithActiveChat {
+  activeChat: number | null;
+}
+
+export interface WithSetActiveChat {
+  setActiveChat: Dispatch<SetStateAction<number | null>>;
+}
+
+export interface ChatItemProps {
+  chat: Chat;
+  deleteChat: (id: number) => Promise<void>;
+}
+
+export interface MessagesProps extends BaseChatProps, WithActiveChat {}
+
+export interface CreateChatProps extends BaseChatProps, WithSetActiveChat {}
+
+export interface ChatListProps
+  extends BaseChatProps,
+    WithActiveChat,
+    WithSetActiveChat {}
+
+export interface MessageInputProps extends BaseSetChats, WithActiveChat {}
