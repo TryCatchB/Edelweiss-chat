d43a95108d9064f12b5bfab74368358b1ebe06de
TryCatchB <pozdrakovzena@gmail.com>
2025-02-17 18:42:27 +0300

    Update CreateChat.tsx

--- client/components/CreateChat.tsx
+++ client/components/CreateChat.tsx
@@ -1,13 +1,7 @@
-import { Chat } from "@/types/chatInterface";
+import { FC, JSX } from "react";
+import { Chat, CreateChatProps } from "@/types/chatInterface";
 import { createChat as createChatAPI } from "@/utils/api";
 import { PlusIcon } from "@heroicons/react/16/solid";
-import React, { Dispatch, FC, JSX, SetStateAction } from "react";
-
-interface CreateChatProps {
-  chats: Chat[];
-  setChats: Dispatch<SetStateAction<Chat[]>>;
-  setActiveChat: Dispatch<SetStateAction<number | null>>;
-}
 
 const CreateChat: FC<CreateChatProps> = (props): JSX.Element => {
   const { chats, setChats, setActiveChat } = props;
@@ -19,28 +13,28 @@ const CreateChat: FC<CreateChatProps> = (props): JSX.Element => {
 
       const createdChat = await createChatAPI(newChatTitle);
 
-      const newChat: Chat = {
-        id: createdChat.id,
-        title: createdChat.title,
-        messages: [],
-      };
+      setChats((prevChats) => {
+        const newChat: Chat = {
+          id: createdChat.id,
+          title: createdChat.title,
+          messages: [],
+        };
+        return [...prevChats, newChat];
+      });
 
-      setChats([...chats, newChat]);
-      setActiveChat(newChat.id);
+      setActiveChat(createdChat.id);
     } catch (error) {
       console.error("Ошибка при создании чата:", error);
     }
   };
 
   return (
-    <>
-      <button
-        onClick={createChat}
-        className="flex items-center bg-blue-500 text-white p-2 w-full mb-2"
-      >
-        <PlusIcon className="h-5 w-5 mr-2" /> Создать чат
-      </button>
-    </>
+    <button
+      onClick={createChat}
+      className="flex items-center bg-blue-500 text-white p-2 w-full mb-2"
+    >
+      <PlusIcon className="h-5 w-5 mr-2" /> Создать чат
+    </button>
   );
 };
 
