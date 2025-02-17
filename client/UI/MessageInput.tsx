17e8916c12f0a09a95bfb67f5932efaf210f7ea3
TryCatchB <pozdrakovzena@gmail.com>
2025-02-17 18:41:34 +0300

    Create MessageInput.tsx

---
+++ client/UI/MessageInput.tsx
@@ -0,0 +1,45 @@
+import { FC, JSX, useState } from "react";
+import { MessageInputProps } from "@/types/chatInterface";
+import { sendMessage } from "@/utils/api";
+
+const MessageInput: FC<MessageInputProps> = (props): JSX.Element => {
+  const { setChats, activeChat } = props;
+  const [message, setMessage] = useState("");
+
+  const handleSendMessage = async () => {
+    if (!message.trim() || !activeChat) return;
+
+    try {
+      const newMessage = await sendMessage(String(activeChat), message);
+      setChats((prevChats) =>
+        prevChats.map((chat) =>
+          chat.id === activeChat
+            ? { ...chat, messages: [...(chat.messages ?? []), newMessage] }
+            : chat
+        )
+      );
+      setMessage("");
+    } catch (error) {
+      console.error("Ошибка при отправке сообщения", error);
+    }
+  };
+
+  return (
+    <>
+      <input
+        className="flex-1 border p-2 text-black"
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+        placeholder="Введите сообщение"
+      />
+      <button
+        onClick={handleSendMessage}
+        className="bg-green-500 text-white p-2 ml-2"
+      >
+        Send
+      </button>
+    </>
+  );
+};
+
+export default MessageInput;
